94af617923106dadc042f5c257e280a22e1d9430
Josue De Almeida Rodrigues <jo.alm.rod@gmail.com>
2023-08-30 12:24:40 +0200

    feat(board): Add new controller method
    Modification of Board and Project Controller
    New partial for new_board & board_card%

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,8 @@
 class BoardsController < ApplicationController
+  before_action :set_project, only: [:create]
 	def index
-		@boards = Board.all
+		@boards = Board.where(project_id: params[:id])
+    @board = Board.new
 	end
 
 	def show
@@ -13,9 +15,11 @@ class BoardsController < ApplicationController
 
 	def create
 		@board = Board.new
-		@board.title = params[:board][:title]
+		@board.name = params[:board][:name]
+    @board.description = params[:board][:description]
+    @board.project_id = params[:project_id]
 		@board.save
-		redirect_to board_path(@board)
+		redirect_to project_path(@project)
 	end
 
 	def edit
@@ -32,6 +36,10 @@ class BoardsController < ApplicationController
 	def destroy
 		@board = Board.find_by(id: params[:id])
 		@board.destroy
-		redirect_to boards_path
+		redirect_to projects_path
 	end
+  def set_project
+    @project = Project.find_by_id(params[:project_id])
+
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,52 +1,54 @@
 class ProjectsController < ApplicationController
-	before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@projects = Project.all
-		@project = Project.new
-	end
+  def index
+    @projects = Project.all
+    @project = Project.new
+  end
 
-	def show
-	end
+  def show
+    @board = Board.new
+    @boards = Board.where(project_id: @project.id)
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def create
-		@project = Project.new(project_params)
-		@project.user = current_user
+  def create
+    @project = Project.new(project_params)
+    @project.user = current_user
 
-		if @project.save
-			redirect_to projects_path, notice: 'Project was successfully created.'
-		else
-			@projects = Project.all
-			render :index, status: :unprocessable_entity
-		end
-	end
+    if @project.save
+      redirect_to projects_path, notice: 'Project was successfully created.'
+    else
+      @projects = Project.all
+      render :index, status: :unprocessable_entity
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @project.update(project_params)
-			redirect_to projects_path, notice: 'Project was successfully updated.'
-		else
-			render :index, status: :unprocessable_entity
-		end
-	end
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path, notice: 'Project was successfully updated.'
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 
-	def destroy
-		@project.destroy
-		redirect_to projects_path
-	end
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
 
-	private
+  private
 
-	def set_project
-		@project = Project.find_by_id(params[:id])
-	end
+  def set_project
+    @project = Project.find_by_id(params[:id])
+  end
 
-	def project_params
-		params.require(:project).permit(:name, :description, :user_id)
-	end
+  def project_params
+    params.require(:project).permit(:name, :description, :user_id)
+  end
 end
