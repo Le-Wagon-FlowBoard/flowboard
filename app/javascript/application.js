8a79dc33e884936550e77a0d3bbc44401a93d70a
Killian' DAL-CIN <killian.dalcin@gmail.com>
2023-09-01 08:35:47 +0200

    fix(stimulus): fix stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,6 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+import muuri_controller from "./muuri_controller"
+window.Stimulus.register("muuri", muuri_controller)
