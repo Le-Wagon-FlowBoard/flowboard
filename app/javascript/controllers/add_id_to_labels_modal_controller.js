c0412f3336f2f678e991b392fdea15b69761fdb0
Killian' DAL-CIN <killian.dalcin@gmail.com>
2023-09-04 21:08:50 +0200

    fix(try): try to continue

--- app/javascript/controllers/add_id_to_labels_modal_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@hotwired/stimulus';
-
-export default class extends Controller {
-    connect() {
-       console.log('add_id_to_labels_modal_controller.js')
-    }
-
-    
-}
