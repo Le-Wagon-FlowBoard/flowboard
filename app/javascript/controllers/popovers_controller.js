2b2a6aa29f07a808be3b4d287a3e1198ae4803d7
Josue De Almeida Rodrigues <jo.alm.rod@gmail.com>
2023-09-07 11:32:26 +0200

    feat(popovers): Set popovers
    %

---
+++ app/javascript/controllers/popovers_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="popovers"
+export default class extends Controller {
+  connect() {
+    new bootstrap.Popover(this.element)
+  }
+}
