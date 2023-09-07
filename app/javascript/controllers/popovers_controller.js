9f3a6bd17c4174b1cab6150685cb21f178a3df49
Josue De Almeida Rodrigues <jo.alm.rod@gmail.com>
2023-09-07 15:10:29 +0200

    feat(popovers): Add popovers for title Board
    Add popovers in the title board which screen description%

--- app/javascript/controllers/popovers_controller.js
+++ app/javascript/controllers/popovers_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="popovers"
 export default class extends Controller {
   connect() {
+    console.log("Hello, Stimulus!", this.element)
     new bootstrap.Popover(this.element)
   }
 }
